862af326b252d0c7d4f86e9df671790581c3de3c
Caio0-0 <126476942+Caio0-0@users.noreply.github.com>
2023-05-15 16:17:19 -0300

    responsivo

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function click(){
-    pesquisa.style.display ="inline"
+const menu = document.querySelector(".menu-lateral")
+const fecharMenu = document.querySelector(".fechar-menu")
+const abrirMenu = document.querySelector("#abrir-menu")
+
+abrirMenu.addEventListener("click",show)
+fecharMenu.addEventListener("click",fechar)
+
+function show(){
+    menu.style.display ="flex"
+    menu.style.left = '0'
 }
+function fechar(){
+    menu.style.left= '-120%'
+}
+
