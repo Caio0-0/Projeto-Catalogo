3f4b5065202311c73f490a1d09bc2d6dd42a0201
Caio0-0 <126476942+Caio0-0@users.noreply.github.com>
2023-05-25 17:16:33 -0300

    add pesquisa

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ function fechar(){
     menu.style.left= '-120%'
 }
 
+const iconeClick = document.querySelector(".icones")
+const pesquisa = document.querySelector(".input-Pesquisa")
+
+function abrirPesquisa(){
+    if (pesquisa.style.display == "none"){
+        pesquisa.style.display ="inline"
+    }else{
+        pesquisa.style.display = "none"
+    }
+}
